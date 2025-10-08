d53f328a4ce12930cf6a1b291fd11bcff7b5b24f
Sunavya-Amujuri <amujurisunavya22@gmail.com>
2025-10-08 12:53:49 +0530

    Time: 0 ms (100%), Space: 8.7 MB (91.02%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int len = s.size();
+        int i = len-1;
+
+        int count = 0;
+        while(i>=0 && s[i] == ' '){
+            i--;
+        }
+
+        while(i>=0 && s[i] != ' '){
+            count++;
+            i--;
+        }
+        return count;
+    }
+};
