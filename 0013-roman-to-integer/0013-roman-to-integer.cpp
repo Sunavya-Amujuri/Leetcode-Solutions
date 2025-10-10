4f5a2923da672269ec59dcca5bd08d6370caee96
Sunavya-Amujuri <amujurisunavya22@gmail.com>
2025-10-10 14:01:34 +0530

    Time: 4 ms (66.78%), Space: 13.4 MB (18.38%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -7,7 +7,7 @@ public:
             {'L', 50}, {'X', 10}, {'V', 5}, {'I', 1}
         };
         for(int i=0; i<s.size(); i++){
-            if(roman[s[i]] < roman[s[i+1]]){
+            if(i + 1 < s.size() && roman[s[i]] < roman[s[i+1]]){
                 total -= roman[s[i]];
             } else {
                 total += roman[s[i]];
