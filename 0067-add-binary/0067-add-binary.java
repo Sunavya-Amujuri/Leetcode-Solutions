ba85f6a4eb2eb6b89b43b19d0e0d32e96e86ae48
Sunavya-Amujuri <amujurisunavya22@gmail.com>
2025-12-12 12:26:06 +0530

    Time: 1 ms (99.84%), Space: 43.8 MB (17.57%) - LeetHub

--- 0067-add-binary/0067-add-binary.java
+++ 0067-add-binary/0067-add-binary.java
@@ -10,18 +10,17 @@ class Solution {
         while(i>=0 || j>=0 || carry==1){
             int sum = carry;
 
-            if(i >= 0){
-                sum += a.charAt(i) - '0';
+            if(i>=0){
+                sum = sum + a.charAt(i) - '0';
                 i--;
             }
-            if(j >= 0){
-                sum += b.charAt(j) - '0';
+            if(j>=0){
+                sum = sum + b.charAt(j) - '0';
                 j--;
             }
             sb.append(sum%2);
             carry = sum/2;
         }
-
         return sb.reverse().toString();
     }
 }
